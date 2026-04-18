c7c4df1ba14c759412732257ea8da85fe953db1f
bmueller1231 <bmueller1231@yahoo.com>
2026-04-18 15:57:12 -0700

    Add letter entry fail test

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -12,13 +12,12 @@ class TestTheGame(unittest.TestCase):
         guess_the_word.reset_game()
     
     def test_word_selection_from_list(self):
-        '''Test that the word comes from a list of values'''
+        """Test that the word comes from a list of values"""
         
-        expected_words = [
-            "BENEFICIAL", "EXCELLENT", "BLACKBERRY", "FACETIOUS",
-            "AREIOUS", "TRAGEDIOUSLY", "VIGOROUSLY", "NOCTURNAL",
-            "TREBUCHET", "POWERFUL", "QUESTIONABLE", "PIZZARIA",
-            "MONUMENTAL", "GALANTRY", "CAPRICIOUS", "VACILLATION"]
+        expected_words =    ["BENEFICIAL", "EXCELLENT", "BLACKBERRY", "FACETIOUS",
+                            "AREIOUS", "TRAGEDIOUSLY", "VIGOROUSLY", "NOCTURNAL",
+                            "TREBUCHET", "POWERFUL", "QUESTIONABLE", "PIZZARIA",
+                            "MONUMENTAL", "GALANTRY", "CAPRICIOUS", "VACILLATION"]
         
         guess_the_word.build_game_word_instance()
         selected_word = "".join(guess_the_word.game_word.values())
@@ -28,17 +27,22 @@ class TestTheGame(unittest.TestCase):
     def test_guess(self):
 
         # Manually set the word to ensure we know the correct letters
-        guess_the_word.game_word = {0: 'W', 1: 'O', 2: 'R', 3: 'D'}
-        guess_the_word.guess_word = {0: '_', 1: '_', 2: '_', 3: '_'}
+        guess_the_word.game_word = {0: "W", 1: "O", 2: "R", 3: "D", 4: "E", 5: "D"}
+        guess_the_word.guess_word = {0: "_", 1: "_", 2: "_", 3: "_", 4: "_", 5: "_"}
         
-        guess_the_word.word_check('W')
-        
+        guess_the_word.word_check("D")
+
         # Check that the letter was added correctly to the correct guesses dicitonary
-        self.assertEqual(guess_the_word.guess_word[0], 'W')
+        # including multiple replacments at once
+        self.assertEqual(guess_the_word.guess_word[3], "D")
+        self.assertEqual(guess_the_word.guess_word[5], "D")
         
         # Check that wrong_guesses did not increment
         self.assertEqual(guess_the_word.wrong_guesses, 0)
 
+    def test_letter_validation(self):
+        validation = guess_the_word.letter_validation("1")
+        self.assertNotEqual(validation,"continue")
 
 
 if __name__ == "__main__":
